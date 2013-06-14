a6c2b98b1ee4cf6b974d785bf6c60b602b1af4a4
Allen Kim <allen.kim@kineticsocial.com>
2013-06-14 18:26:26 -0400

    changed options of initialSequence to requireFirstFetch

--- README.md
+++ README.md
@@ -22,16 +22,15 @@ MIT licensed
 
 ### Configuration options:
 
- * loadingHtml:     Default, `<div class='loading'>Loading</div>` , Html indicating that ajax loging is in progress
-                  This will be shown when loading start and disappear when it is done.
- * bottomDistance:  The distance from bottom of the element to fire next page loading, default 50px
- * initialSequence: Defualt 2, Starting number of page for ajax loading
- * url:             Url to retrieve data without parameters, i.e. http://my.site.com/posts
- * params:          A function to generate ajax call data hash. The sequence number is passed as parameter 
-                  Default, function(sequence) { return {page: sequence}; },
- * cancelSend:      Callback function when ajax call is successful. data, status, as xhr are passed as parameters
- * beforeSend:      Callback function before ajax call is executed. xhr is passed as parameters
- * complete:        Callback function when ajax call is finished regardless of error or success. xhr and status are passed as parameters
- * error:           Callback function when ajax call is errored out. xhr and status are passed as parameters
- * success:         Callback function when ajax call is successful. data, status, and xhr are passed as parameters
-
+ *  loadingHtml:       Default, "<div class='loading'>Loading</div>", Html indicating that ajax loging is in progress
+ *                     This will be shown when loading start and disappear when it is done.
+ *  bottomDistance:    The distance from bottom of the element to fire next page loading, default 50px
+ *  requireFirstFetch: Defualt, false. Indicating to fetch data when initialzed
+ *  url:               Url to retrieve data without parameters, i.e. http://my.site.com/posts
+ *  params:            A function to generate ajax call data hash. The sequence number is passed as parameter 
+ *                     Default, function(sequence) { return {page: sequence}; },
+ *  cancelFetch:       Callback function when ajax call is successful. data, status, as xhr are passed as parameters
+ *  beforeFetch:       Callback function before ajax call is executed. xhr is passed as parameters
+ *  complete:          Callback function when ajax call is finished regardless of error or success. xhr and status are passed as parameters
+ *  error:             Callback function when ajax call is errored out. xhr and status are passed as parameters
+ *  success:           Callback function when ajax call is successful. data, status, and xhr are passed as parameters
--- jquery.pageless_scroll.js
+++ jquery.pageless_scroll.js
@@ -17,18 +17,18 @@
  *
  * Configuration options:
  *
- *  loadingHtml:     Default, "<div class='loading'>Loading</div>", Html indicating that ajax loging is in progress
- *                   This will be shown when loading start and disappear when it is done.
- *  bottomDistance:  The distance from bottom of the element to fire next page loading, default 50px
- *  initialSequence: Defualt 2, Starting number of page for ajax loading
- *  url:             Url to retrieve data without parameters, i.e. http://my.site.com/posts
- *  params:          A function to generate ajax call data hash. The sequence number is passed as parameter 
- *                   Default, function(sequence) { return {page: sequence}; },
- *  cancelSend:      Callback function when ajax call is successful. data, status, as xhr are passed as parameters
- *  beforeSend:      Callback function before ajax call is executed. xhr is passed as parameters
- *  complete:        Callback function when ajax call is finished regardless of error or success. xhr and status are passed as parameters
- *  error:           Callback function when ajax call is errored out. xhr and status are passed as parameters
- *  success:         Callback function when ajax call is successful. data, status, and xhr are passed as parameters
+ *  loadingHtml:       Default, "<div class='loading'>Loading</div>", Html indicating that ajax loging is in progress
+ *                     This will be shown when loading start and disappear when it is done.
+ *  bottomDistance:    The distance from bottom of the element to fire next page loading, default 50px
+ *  requireFirstFetch: Defualt, false. Indicating to fetch data when initialzed
+ *  url:               Url to retrieve data without parameters, i.e. http://my.site.com/posts
+ *  params:            A function to generate ajax call data hash. The sequence number is passed as parameter 
+ *                     Default, function(sequence) { return {page: sequence}; },
+ *  cancelFetch:       Callback function when ajax call is successful. data, status, as xhr are passed as parameters
+ *  beforeFetch:       Callback function before ajax call is executed. xhr is passed as parameters
+ *  complete:          Callback function when ajax call is finished regardless of error or success. xhr and status are passed as parameters
+ *  error:             Callback function when ajax call is errored out. xhr and status are passed as parameters
+ *  success:           Callback function when ajax call is successful. data, status, and xhr are passed as parameters
  */
 
 ( function($){
@@ -37,11 +37,11 @@
     var defaults = {
       loadingHtml: "<div class='loading'>Loading</div>",
       bottomDistance: 50,
-      initialSequence: 2,
+      requireFirstFetch: false,
       url:         null,
       params:      function(sequence) { return {page: sequence}; },
-      cancelSend:  function() {},
-      beforeSend:  function() {},
+      cancelFetch: function() {},
+      beforeFetch: function() {},
       complete:    function() {},
       error:       function() {},
       success:     function() {}
@@ -57,41 +57,35 @@
         beforeSend: function(xhr) {      
           self.ajaxStatus = LOADING;
           $(self).append(options.loadingHtml);
-          options.beforeSend.apply(self, [xhr]);
+          options.beforeFetch.apply(self, [xhr]);
         }
       }).done( function(data, status, xhr) {
-        self.ajaxStatus = SUCCESS;
-        if (data == []) self.ajaxStatus = ALL_DONE;
+        self.ajaxStatus = (Array.isArray(data) && data.length == 0) ? ALL_DONE : SUCCESS;
         options.success.apply(self, [data, status, xhr]);
       }).fail( function(xhr, status) {
         self.ajaxStatus = ERROR;
-        options.error.apply(this, [data, status, xhr]);
+        options.error.apply(this, [xhr, status]);
       }).always( function(xhr, status) {
         options.complete.apply(self, [xhr, status]);
         $(".loading",self).remove();
       });
     }
 
-    this.sequence = options.initialSequence;
-    if (options.initialSqeuence == 1) { // if we need to load the first page without scrolling
-      loadData.apply(this, [this.sequence]);
-      this.sequence++;
-    } else {
-      if (this == document) 
-        throw "no support for document level for simplicity, use an element. i.e. div";
-      else if ( $(this)[0].scrollHeight == $(this)[0].clientHeight )
-        throw "element is not scrollable, need to have overflow:scroll or equivalent";
+    if (options.requireFirstFetch) { // if we need to load the first page without scrolling
+      loadData.apply(this, [1]);
+    } else if (this == document) {
+      throw "no support for document level for simplicity, use an element. i.e. div";
     }
-
-    var that = this;
+      
+    $(this).data("sequence", 2); //when scrolled, will fetch the second page
     $(this).unbind("scroll").bind("scroll", function() {
       var closeToBottom = ( this.scrollHeight - $(this).height() < $(this).scrollTop() + options.bottomDistance );
-      var cancel = options.cancelSend.apply(this);
+      var cancel = options.cancelFetch.apply(this);
 
       if ([LOADING, ALL_DONE, ERROR].indexOf(this.ajaxStatus) >= 0 || cancel == true) {
       } else if ( closeToBottom ) {
-        loadData.apply(this, [that.sequence]);
-        that.sequence++;
+        loadData.apply(this, [$(this).data("sequence")]);
+        $(this).data("sequence", $(this).data("sequence")+1);
       }
     });
   };
